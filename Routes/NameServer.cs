dd990f0656cee48ae08b8756c606fa4de55e486a
coastgt23 <teddie250214@gmail.com>
2026-06-13 10:41:15 +0100

    Update NameServer and launch settings

--- Routes/NameServer.cs
+++ Routes/NameServer.cs
@@ -9,24 +9,24 @@ namespace Stella.Routes
         {
             return new NameServerResponse
             {
-                Accounts = "http://localhost:3003",
-                API = "http://localhost:3003", // dont add /api because every call to this service to this already does
-                Auth = "http://localhost:3003/auth",
-                CDN = "http://localhost:3003/cdn",
-                Chat = "http://localhost:3003/chat",
-                Clubs = "http://localhost:3003/clubs",
-                Commerce = "http://localhost:3003/commerce",
-                DataCollection = "http://localhost:3003/datacollection",
-                Images = "http://localhost:3003/img",
-                Leaderboard = "http://localhost:3003",
-                Link = "http://localhost:3003/link",
-                Matchmaking = "http://localhost:3003/match",
-                Moderation = "http://localhost:3003/moderation",
-                Notifications = "http://localhost:3003/notify",
-                PlatformNotifications = "http://localhost:3003/platformnotifications",
-                RoomComments = "http://localhost:3003/roomcomments",
-                Rooms = "http://localhost:3003/rooms",
-                Storage = "http://localhost:3003/storage",
+                Accounts = "https://lithiumservers.fly.dev",
+                API = "https://lithiumservers.fly.dev", // dont add /api because every call to this service to this already does
+                Auth = "https://lithiumservers.fly.dev/auth",
+                CDN = "https://lithiumservers.fly.dev/cdn",
+                Chat = "https://lithiumservers.fly.dev/chat",
+                Clubs = "https://lithiumservers.fly.dev/clubs",
+                Commerce = "https://lithiumservers.fly.dev/commerce",
+                DataCollection = "https://lithiumservers.fly.dev/datacollection",
+                Images = "https://lithiumservers.fly.dev/img",
+                Leaderboard = "https://lithiumservers.fly.dev",
+                Link = "https://lithiumservers.fly.dev/link",
+                Matchmaking = "https://lithiumservers.fly.dev/match",
+                Moderation = "https://lithiumservers.fly.dev/moderation",
+                Notifications = "https://lithiumservers.fly.dev/notify",
+                PlatformNotifications = "https://lithiumservers.fly.dev/platformnotifications",
+                RoomComments = "https://lithiumservers.fly.dev/roomcomments",
+                Rooms = "https://lithiumservers.fly.dev/rooms",
+                Storage = "https://lithiumservers.fly.dev/storage",
                 WWW = "http://stellaonline.org"
             };
         }
